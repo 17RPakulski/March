91dc195af78a6489940f5d50d6b09e1dcc0d6c2f
Robert <92167234+17RPakulski@users.noreply.github.com>
2022-03-11 15:14:58 +0000

    Add files via upload

---
+++ 11 ...py
@@ -0,0 +1,14 @@
+import serial
+import time
+
+
+
+serialPort=serial.Serial(port = 'COM3', baudrate=115200,
+bytesize=8,timeout=2,stopbits=serial.STOPBITS_ONE)
+
+
+
+serialString='gAy\n'
+while(1):
+    serialPort.write(serialString.encode())
+    time.sleep(30)
